e64bd0dffe07954591211d5db9f6e7253864a472
Conor Sontag <conor.sontag@ipsos.com>
2014-03-24 18:15:17 -0700

    Use Beautiful Soup to parse out elements of the web page.

--- Scripts/ScrapeData.py
+++ Scripts/ScrapeData.py
@@ -2,4 +2,21 @@
 # Script for grabbing out data of interest from 
 # combine result web pages. 
 
-with open()
+import os
+from bs4 import BeautifulSoup
+
+with open("../Data/nflcombineresults.com/2013draftfile.html") as file:
+	test = file.read()
+
+# Create a beautiful soup object.
+soup = BeautifulSoup(test)
+
+table = soup.table
+
+iter = 0
+for td in table.find_all('td'):
+	if(iter > 40):
+		break
+	else:
+		print td.get_text()
+		iter += 1
