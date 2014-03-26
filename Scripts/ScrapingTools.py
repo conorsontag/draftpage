b01fb031e5260a37ee33c037a6f91d715eb32f05
Conor Sontag <conor.sontag@ipsos.com>
2014-03-26 10:57:49 -0700

    Edits to create data-processed CSVs.

--- Scripts/ScrapingTools.py
+++ Scripts/ScrapingTools.py
@@ -2,7 +2,9 @@
 # Scraping Functions for grabbing 
 # draft data. 
 
-import urllib
+import urllib, csv
+from bs4 import BeautifulSoup
+
 
 def draftScrape(webpage,outfile):
 	# Function to download the source HTML of a web-page
@@ -23,14 +25,74 @@ def draftScrape(webpage,outfile):
 	pageList = download(webpage)
 	outputPage(pageList, outfile)
 
-# Scrape Web Pages
+def parseCombineResults(infile,year):
+	newStr = []
+	database = []
+
+	with open(infile,'r') as f:
+		test = f.read()
+
+	soup = BeautifulSoup(test)
+	table = soup.table
+	# Transform html file into a dataset...
+	for td in table.find_all('td'):
+		text = td.get_text()
+		try:
+			if(text == str(year)):
+				database.append(newStr)
+				newStr = [text]
+			else:
+				newStr.append(text)
+		except Exception:
+			print text
+			raise "Error in parsing"
+	return database
+
+# Scrape Web Pages  - NFL combineresults .com
 pageString = "http://nflcombineresults.com/nflcombinedata.php?year=2013&pos=&college="
 
-years = xrange(1999,2014)
+years = xrange(1999,2015)
 
 for i in years:
 	yearStr = pageString.replace("2013", str(i))
 	print yearStr
-	output = str(i) + '..\Data\draftfile.html'
+	output = '../Data/nflcombineresults.com/' + str(i) + 'draftfile.html'
 	draftScrape(yearStr, output)
 
+
+# From 
+pageString = "http://www.cbssports.com/nfl/draft/combine/XXXX/ALL/ALL/?print_rows=9999"
+
+years = xrange(2002,2015)
+
+for i in years:
+	yearStr = pageString.replace("XXXX", str(i))
+	print yearStr
+	output = '../Data/CBS Results/' + str(i) + 'draftfile.html'
+	draftScrape(yearStr, output)
+
+
+#-------------------------------------------
+# After parsing write each years worth of 
+# data to file.
+
+csvDict = {}
+years = xrange(1999,2015)
+
+i = 1999
+for i in years:
+	inFile = '../data/nflcombineresults.com/' + str(i) + 'draftfile.html'	
+	csvDict[i] = parseCombineResults(inFile,i)
+
+for key in csvDict.keys():
+	outName = '../Data/processedData_' + str(key) + '.csv'
+	with open(outName,'wb') as outfile:
+		fileWriter = csv.writer(outfile)
+		for line in csvDict[key]:
+			fileWriter.writerow(line)
+
+
+with open(inFile) as f:
+	x = f.read()
+
+print x[0:5]
